f6cc78521238e019db8fc77c8ca24807045458db
Ernest Nnamdi <ernest@Ernests-MacBook-Air.local>
2021-10-11 00:05:57 +0100

    first commit

---
+++ screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+
+const Home = () => {
+    return (
+        <SafeAreaView>
+            <Text>Homeeeeeeee bishhhhh</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Home
